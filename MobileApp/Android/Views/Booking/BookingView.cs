f0c7579c41950583c452645a8c49578d2bd9b0ec
fadeev <black_pol@mail.ru>
2019-01-14 20:37:06 +0300

    add, android - bookingView for android has been added

---
+++ MobileApp/Android/Views/Booking/BookingView.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using MvvmCross.Platforms.Android.Views;
+using TransportSystems.Frontend.App.ViewModels.Booking;
+
+namespace TransportSystems.Frontend.MobileApp.Android.Views.Booking
+{
+    [Activity(Label = "BookingView", WindowSoftInputMode = SoftInput.StateHidden)]
+    public class BookingView : MvxActivity<BookingViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.BookingView);
+        }
+
+    }
+}
