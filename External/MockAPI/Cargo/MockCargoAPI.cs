b591d7edfb0f668752da1ec883fe82cef2eea989
fadeev <black_pol@mail.ru>
2019-06-08 10:28:05 +0300

    рефатокторинг

--- External/MockAPI/Cargo/MockCargoAPI.cs
+++ External/MockAPI/Cargo/MockCargoAPI.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using TransportSystems.Frontend.External.Interfaces.Cargo;
 using TransportSystems.Frontend.External.Models.Cargo;
